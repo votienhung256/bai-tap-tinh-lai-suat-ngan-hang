152013ba0d389711ffbc5d5346b4652af7c6fea9
Your Name <you@example.com>
2021-03-06 20:10:37 +0700

    Hoàn thành bài tập

---
+++ input.js
@@ -0,0 +1,6 @@
+let money = prompt("Nhập số tiền (VNĐ): ");
+money = Number(money);
+let lai = prompt("Nhập lãi suất (%): ");
+lai = Number(lai);
+let year = prompt("Nhập số năm: ");
+year = Number(year);
---
+++ output.js
@@ -0,0 +1,3 @@
+let tien_lai = money * (lai / 100);
+alert(`Số tiền lãi sau 1 năm là ${tien_lai} VNĐ`);
+alert(`Số tiền cả gốc lẫn lãi sau ${year} năm là: ${money + (year * tien_lai)} VNĐ`);
